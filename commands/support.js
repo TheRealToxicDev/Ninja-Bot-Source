a91825b43ca30df6dd984dc7b923c20c3152cb3d
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-25 03:06:56 -0600

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 exports.run = (client, message, args) => {
-        message.channel.send(`Support server: https://discord.gg/NeeaqnC`);
+        message.channel.send(`Support server: https://ninjabot.site/discord`);
 }
    
  
